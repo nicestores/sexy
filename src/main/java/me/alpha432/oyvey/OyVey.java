dfc2118613f8b1bcc05773c0892a4ecd75eebeac
Bypass <jadecore@proton.me>
2025-07-05 15:23:48 +0300

    Update OyVey.java

--- src/main/java/me/alpha432/oyvey/OyVey.java
+++ src/main/java/me/alpha432/oyvey/OyVey.java
@@ -8,8 +8,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class OyVey implements ModInitializer, ClientModInitializer {
-    public static final String NAME = "OyVey";
-    public static final String VERSION = "0.0.3 - " + SharedConstants.getGameVersion().getName();
+    public static final String NAME = "JewClient";
+    public static final String VERSION = "1.9.44 - " + SharedConstants.getGameVersion().getName();
 
     public static float TIMER = 1f;
 
